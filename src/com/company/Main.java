cc018d3f8f143764918896ea6b28c32ffb92391c
Регина <rmashkovtseva@gmail.com>
2023-01-24 17:35:35 +0300

    Подготовлен класс с продуктами. Сделан массив с имеющимися продуктами в магазине

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
 public class Main {
+    public static int maxProduct = 10;
+    public static int numberProduct = 0;
+    public static Product[] products = new Product[maxProduct];
+
+    public static void addShop(Product product){
+        products[numberProduct] = product;
+        numberProduct++;
+    }
 
     public static void main(String[] args) {
-	// write your code here
+	    Product bread = new Product("Хлеб", 30);
+        Product banana = new Product("Банан", 7);
+        Product oil = new Product("Масло", 100);
+        Product cookies = new Product ("Печенье", 70);
+        Product fish = new Product ("Рыба", 150);
+        Product meat = new Product ("Мясо", 200);
+
+
+
     }
 }
